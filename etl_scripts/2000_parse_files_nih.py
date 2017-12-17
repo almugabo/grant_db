9ecc7c0847c973470842ce24e497ebeadfe2a0fc
mike <almugabo@gmail.com>
2017-12-17 21:22:00 +0100

    create file with functions for data acquisition

--- etl_scripts/2000_parse_files_nih.py
+++ etl_scripts/2000_parse_files_nih.py
@@ -24,7 +24,6 @@ xDBCon = 'sqlite:///' + xDB
 
 xFld = '/media/mike/MyDataContainer/1000_ScientoMetricData/___Staging/1000_GRANTS/1000_dset_original/4000_NIH/_CSV/'
 
-# In[19]:
 
 # IMPORT THE TABLES -- projects
 print '--- starting', time.ctime()
@@ -52,5 +51,29 @@ for x_year in range(2006, 2017):
 
 print '--------all done---', time.ctime()
 
+# abstracts
 
+print '--- starting', time.ctime()
+for x_year in range(2006, 2017):
+    x_year_str = str(x_year)
+    print '-------------------- process: ', x_year_str
+    xFile_Zipped = xFld + x_year_str + '_abstract.zip'
+    xFile_nonZipped = 'RePORTER_PRJABS_C_FY' + x_year_str + '.csv'
+    xTabName = 'nih_' + x_year_str + 'projects_abst'
 
+    # Process File
+    df1 = None
+    with zipfile.ZipFile(xFile_Zipped) as z:
+        with z.open(xFile_nonZipped) as f:
+            df1 = pd.read_csv(f, low_memory=False, encoding='latin-1')  # , nrows=100)
+            # print len(df1)
+
+            df1.to_sql(name=xTabName,
+                       con=xDBCon,
+                       if_exists='replace',
+                       index=True, index_label='record_id',
+                       chunksize=10000)
+
+            print 'saved ! records:', len(df1), time.ctime()
+
+print '--------all done---', time.ctime
