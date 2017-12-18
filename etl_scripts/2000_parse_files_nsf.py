8862383fbbb678ac2d5c5980eb4725aeca4471c5
mike <almugabo@gmail.com>
2017-12-18 08:26:48 +0100

    create file with functions for data acquisition

--- etl_scripts/2000_parse_files_nsf.py
+++ etl_scripts/2000_parse_files_nsf.py
@@ -4,39 +4,50 @@
 
 # # parsing grant files   NSF
 
-# add parse function
+import time
 import sys
+from sqlalchemy import create_engine
+import zipfile
+import os
+import pandas as pd
+
+# add parse function
 xPath_package = '/home/mike/PycharmProjects/grant_db/'
 sys.path.append(xPath_package)
 
-import pandas as pd
-import os
+# connection strings
+# xPGConnString = 'postgresql://postgres:post@localhost:5432/eris'
+# xDBCon = create_engine(xPGConnString)
 
-#from etl import etl_utils as UT
-from etl import etl_parse_nsf as P_NSF
-
-
-# PATH to the parsed results
-xFld_Path_parsed = '/media/mike/SSD_Data/__data_staging/1000_grant_db/2000_dset_parsed/'
-
-xDB = xFld_Path_parsed + 'grant_data_parsed.db'
+xDB = '/media/mike/MyDataContainer/1000_ScientoMetricData/___Staging/1000_GRANTS/2000_dset_parsed/grant_dset.db'
 xDBCon = 'sqlite:///' + xDB
 
-
-# PATH to datasets
-xFld_Path_dset_original = '/media/mike/SSD_Data/__data_staging/1000_grant_db/1000_dset_original/'
-xFld_Path_set = xFld_Path_dset_original + '5000_NSF/'
-
-xlst_Files = os.listdir(xFld_Path_set)
-xFile = xlst_Files[0]
-xfile_name = xFld_Path_set  + xFile
-xfile_name
+# # NSF
 
 
+from etl import etl_parse_nsf as p_nsf
 
-xq1 = P_NSF.parse_nsf_grant_to_csv(xfile_name )
 
-xq1.keys()
+xFld = '/media/mike/MyDataContainer/1000_ScientoMetricData/___Staging/1000_GRANTS/1000_dset_original/5000_NSF/'
 
-df1 = xq1['grant_info']
-df1.head()
+
+print '! starting', time.ctime()
+
+for x_year in range(2000, 2017):
+    x_year_str = str(x_year)
+    xFile_Zipped = xFld + x_year_str + '.zip'
+    print '--process', x_year_str
+
+    xq1 = p_nsf.parse_nsf_grant_to_csv(xFile_Zipped)
+
+    for xkey in xq1.keys():
+        xTabName = 'nsf_' + x_year_str + '_' + xkey
+
+        (xq1[xkey]).to_sql(name=xTabName,
+                           con=xDBCon,
+                           if_exists='replace',
+                           index=True, index_label='record_id',
+                           chunksize=10000)
+        print '------saved', xTabName, 'records:', len(xq1[xkey]), time.ctime()
+
+print '-- all done', time.ctime()
